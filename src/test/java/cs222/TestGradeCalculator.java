6ef5af27ba5d93570298f4af38c2029c96bd63bf
maxtharp <maxwelltharp7@gmail.com>
2024-03-29 11:55:13 -0400

    Passing tests

--- src/test/java/cs222/TestGradeCalculator.java
+++ src/test/java/cs222/TestGradeCalculator.java
@@ -6,36 +6,36 @@ import org.junit.jupiter.api.Test;
 public class TestGradeCalculator {
     @Test
     public void testA(){
-        GradeCalculator gradeCalculator = new GradeCalculator();
-        char grade = gradeCalculator.calculate();
+        Calculator calculator = new Calculator();
+        char grade = calculator.calculate("AAAAA".toCharArray());
         Assertions.assertEquals(grade, 'A');
     }
 
     @Test
     public void testB(){
-        GradeCalculator gradeCalculator = new GradeCalculator();
-        char grade = gradeCalculator.calculate();
+        Calculator calculator = new Calculator();
+        char grade = calculator.calculate("AABAA".toCharArray());
         Assertions.assertEquals(grade, 'B');
     }
 
     @Test
     public void testC(){
-        GradeCalculator gradeCalculator = new GradeCalculator();
-        char grade = gradeCalculator.calculate();
+        Calculator calculator = new Calculator();
+        char grade = calculator.calculate("CCCCB".toCharArray());
         Assertions.assertEquals(grade, 'C');
     }
 
     @Test
     public void testD(){
-        GradeCalculator gradeCalculator = new GradeCalculator();
-        char grade = gradeCalculator.calculate();
+        Calculator calculator = new Calculator();
+        char grade = calculator.calculate("ADBDD".toCharArray());
         Assertions.assertEquals(grade, 'D');
     }
 
     @Test
     public void testF(){
-        GradeCalculator gradeCalculator = new GradeCalculator();
-        char grade = gradeCalculator.calculate();
+        Calculator calculator = new Calculator();
+        char grade = calculator.calculate("FFFFA".toCharArray());
         Assertions.assertEquals(grade, 'F');
     }
 
