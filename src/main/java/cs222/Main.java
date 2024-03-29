8685d0ad5a992effe49e3f34d675965ddac0bfc9
maxtharp <maxwelltharp7@gmail.com>
2024-03-29 12:02:53 -0400

    Main class

---
+++ src/main/java/cs222/Main.java
@@ -0,0 +1,15 @@
+package cs222;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ConvertToLetters gradeCalculator = new ConvertToLetters();
+        Calculator calculator = new Calculator();
+        char[] inputGrades = gradeCalculator.getLetterGrades();
+        System.out.println(inputGrades);
+
+        System.out.println("You earned a(n) " + calculator.calculate(inputGrades) +
+                " in CS222.");
+    }
+}
+
